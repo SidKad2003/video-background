1e58933b5d6fb378bdf9e16ceafb4e78948069e8
Siddharth <sidkaduskar@gmail.com>
2023-07-30 18:02:43 +0530

    checking for threeJS

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Itenary from "./components/additions/Itenary";
 import Landingpage from "./components/landingpage";
 import React from "react";
 import './components/landingpage.css'
-import BgVideo from './components/media/bg.mp4'
+// import BgVideo from './components/media/bg.mp4'
 
 const Container = styled.div`
   height: 100vh;
@@ -23,7 +23,7 @@ const Container = styled.div`
   scroll-behavior: smooth;
   overflow-y: auto;
   scrollbar-width: none;
-  color: black;
+  background-color: black;
   &::-webkit-scrollbar {
     display: none;
   }
@@ -33,8 +33,8 @@ function App() {
   return (
     <>
       <div className="landingpage">
-        <video src={BgVideo} autoPlay muted loop class="video-bg" />
-        <div className="bg-overlay"></div>
+        {/* <video src={BgVideo} autoPlay muted loop class="video-bg" /> */}
+        {/* <div className="bg-overlay"></div> */}
         <div className="home-text">
           <Container>
             {/* Registration Prompt Mujherjee */}
@@ -52,8 +52,8 @@ function App() {
             {/* Past Events Archives HARSH */}
             <Past />
             {/* FAQs Sid */}
-            {/* <Faq /> */}
-            {/* <Works /> */}
+            <Faq />
+            <Works />
           </Container>
         </div>
       </div>
